316a3d86ed12ef4ef32f338fee9aad3d0d7f7020
endam1234 <mckennen@tcd.ie>
2018-11-05 17:18:52 +0000

    Update Ass4_githubAPI.R
    Commit

---
+++ Ass4_githubAPI.R
@@ -0,0 +1,40 @@
+install.packages("jsonlite")
+
+library(jsonlite)
+
+install.packages("httpuv")
+library(httpuv)
+
+install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "mckennen",
+                   key = "185861e816e2f990d814",
+                   secret = "007e15efaa7f48e328d64b1617b1da69a1c5ca4e")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+#link: https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
+
+
